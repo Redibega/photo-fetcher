0d3d603706d1b887ee00fd14fb87ed6a60dedba5
Redibega <119885308+Redibega@users.noreply.github.com>
2024-01-22 13:26:13 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+const photoContainer = document.getElementById("photoContainer");
+const toggleSwitch = document.getElementById("toggleSwitch");
+const loadMoreBtn = document.getElementById("loadMoreBtn");
+function fetchPhotos() {
+  // Clear the existing content of the photo container
+  photoContainer.innerHTML = "";
+
+  // Fetch 4 random photos from the API
+  for (let i = 0; i < 4; i++) {
+    fetch("https://picsum.photos/367/367")
+      .then((response) => {
+        if (response.ok) {
+          return response.url;
+        } else {
+          throw new Error("Failed to fetch photo");
+        }
+      })
+      .then((photoUrl) => {
+        // Create an image element
+        const img = document.createElement("img");
+        img.src = photoUrl;
+        img.alt = "Random Photo";
+
+        // Append the image to the container
+        photoContainer.appendChild(img);
+      })
+      .catch((error) => console.error(error));
+  }
+}
+
+function applyGrayscale() {
+  // Apply grayscale class to all images if the toggle switch is enabled
+  const images = photoContainer.querySelectorAll("img");
+  images.forEach((img) => {
+    if (toggleSwitch.checked) {
+      img.classList.add("grayscale");
+    } else {
+      img.classList.remove("grayscale");
+    }
+  });
+}
+
+// Initial load of photos
+fetchPhotos();
+
+// Apply grayscale effect when the switch is changed
+toggleSwitch.addEventListener("change", function () {
+  applyGrayscale();
+});
+
+loadMoreBtn.addEventListener("click", fetchPhotos);
